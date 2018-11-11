e89bb41dffb745fd7c32cf4ffd05205ac81886f3
jfgilliam@yahoo.com <jfgilliam@yahoo.com>
2018-11-11 10:53:18 -0500

    Fixed issues with TrackMap at COTA (Circuit of the Americas). Finish Line in wrong place (Grand Prix,West) Pit Road not mapped correctly (East).

--- src/build-track-patch.bat
+++ src/build-track-patch.bat
@@ -1,8 +1,8 @@
 @echo off
 cd
 @echo on
-set TRACK=bullring
+set TRACK=cota
 del %TRACK%.zip %TRACK%.sra
-7z a -bb1 -tzip -r %TRACK% %TRACK%.json %TRACK%-*
+7z a -bb1 -tzip -r %TRACK% %TRACK%.json %TRACK%*
 rename %TRACK%.zip %TRACK%.sra
 pause
